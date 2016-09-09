e06ceba34a7e92abdb27476da84f0e02f2b790a7
sr <skyrousse@gmail.com>
2016-09-09 15:44:18 -0700

    add method and view to update user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-
+    @user = User.find(params[:id])
   end
 
   def new
@@ -18,10 +18,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+   @user = User.find(params[:id])
+   if @user.update(user_params_update)
+     flash[:notice] = "User info successfully updated!"
+     redirect_to user_path(@user)
+   else
+     render :show
+   end
+ end
+
 private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user, :email, :password)
+  end
+
+  def user_params_update
+    params.require(:user).permit(:email, :name, :about, :profile_pic, :skills, :zip, :likes_to)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :pets
-  validates :password, :length => { :minimum => 8 }
+  validates :password, :length => { :minimum => 8 }, :if => :password
   validates :email, :presence => true, :uniqueness => true
   before_save :downcase_fields
 
